50a16ff800478ad85ecf82f17b0c3efb0444c2d7
Dan Brima <brima.dan@gmail.com>
2022-06-13 22:53:55 +0300

    feat(entire project): working

--- extension.js
+++ extension.js
@@ -1,7 +1,117 @@
 const vscode = require("vscode");
+const path = require("path");
 // @ts-ignore
 var { types } = require("conventional-commit-types");
 const { spawnSync } = require("child_process");
+
+const confirmButton = {
+  iconPath: new vscode.ThemeIcon("arrow-right"),
+  tooltip: "confirm",
+};
+
+function createSimpleQuickPick({
+  placeholder,
+  items = [],
+  activeItems = [],
+  value = "",
+  buttons = [],
+}) {
+  return new Promise(function (resolve, reject) {
+    const picker = vscode.window.createQuickPick();
+    picker.placeholder = placeholder;
+    picker.matchOnDescription = true;
+    picker.matchOnDetail = true;
+    picker.ignoreFocusOut = true;
+    picker.items = items;
+    picker.activeItems = activeItems.length === 0 ? [items[0]] : activeItems;
+    picker.value = value;
+    picker.show();
+    picker.buttons = [...buttons, confirmButton];
+    picker.onDidAccept(function () {
+      if (picker.activeItems.length) {
+        resolve({
+          value: picker.value,
+          activeItems: picker.activeItems,
+        });
+        picker.dispose();
+      }
+    });
+    picker.onDidTriggerButton(function (e) {
+      if (e === confirmButton) {
+        if (picker.activeItems.length) {
+          resolve({
+            value: picker.value,
+            activeItems: picker.activeItems,
+          });
+        } else {
+          resolve({
+            value: picker.value,
+            activeItems: [picker.items[0]],
+          });
+        }
+        picker.dispose();
+      }
+
+      if (e === vscode.QuickInputButtons.Back) {
+        reject({
+          button: e,
+          value: picker.value,
+          activeItems: picker.activeItems,
+        });
+      }
+    });
+  });
+}
+
+function hasChanges(repo) {
+  return (
+    repo.state.workingTreeChanges.length ||
+    repo.state.mergeChanges.length ||
+    repo.state.indexChanges.length
+  );
+}
+
+async function getRepository({ git, arg, workspaceFolders }) {
+  const _arg = arg?._rootUri.fsPath;
+
+  if (_arg) {
+    const repo = git.repositories.find(function (r) {
+      return r.rootUri.fsPath === _arg;
+    });
+    if (repo) return repo;
+    else {
+      console.error("getRepository", true);
+    }
+  }
+
+  if (git.repositories.length === 0) {
+    console.error("getRepository", true);
+  }
+
+  if (git.repositories.length === 1) return git.repositories[0];
+
+  const items = git.repositories.map(function (repo, index) {
+    const folder = workspaceFolders?.find(function (f) {
+      return f.uri.fsPath === repo.rootUri.fsPath;
+    });
+    return {
+      index,
+      label: folder?.name || path.basename(repo.rootUri.fsPath),
+      description:
+        (repo.state.HEAD?.name || repo.state.HEAD?.commit?.slice(0, 8) || "") +
+        (hasChanges(repo) ? "*" : ""),
+    };
+  });
+
+  const [{ index }] = (
+    await createSimpleQuickPick({
+      placeholder: "Choose a repository:",
+      items,
+    })
+  ).activeItems;
+
+  return git.repositories[index];
+}
 
 const commitTypeToVsOption = ([commitType, { title, description }]) => ({
   commitType,
@@ -29,42 +139,76 @@ const getBuiltInGitApi = async () => {
 const formatCommit = (commitType, affected, commitMessage) =>
   `${commitType}(${affected}): ${commitMessage}`;
 
-const commit = async () => {
-  const api = await getBuiltInGitApi();
-  const rootPath = vscode.workspace.workspaceFolders;
-  console.log({ rootPath, a: api.repositories });
-  // const repository = api.repositories.filter(r => isDescendant(r.rootUri.fsPath, rootPath))[0];
+const throwIfEmptyElseReturn = (string) => {
+  if (string === undefined) {
+    throw "None of the options above were selected";
+  }
 
-  const options = Object.entries(types).map(commitTypeToVsOption);
-  options[0].picked = true;
+  return string;
+};
 
-  const { commitType } = await vscode.window.showQuickPick(options);
+const showQuickPick = async (options) => {
+  const option = await vscode.window.showQuickPick(options);
 
-  const affected = await vscode.window.showInputBox({
-    title: "Files or areas affected",
-    value: "entire project",
-    prompt: "e.g Main, Navbar",
-  });
+  return throwIfEmptyElseReturn(option);
+};
 
-  const commitMessage = await vscode.window.showInputBox({
-    title: "Commit description",
-    prompt: "Write a short, imperative tense description of the change",
-    validateInput(commitDescription) {
-      return commitDescription.length === 0 && "subject is required";
-    },
-  });
+const showInputBox = async (options) => {
+  const option = await vscode.window.showInputBox(options);
 
-  await vscode.commands.executeCommand("git.commit");
+  return throwIfEmptyElseReturn(option);
+};
 
-  vscode.window.showInformationMessage("Hello World from bsmch-commits!");
+const commit = async (args) => {
+  try {
+    const api = await getBuiltInGitApi();
+
+    const repository = await getRepository({
+      arg: args,
+      git: api,
+      workspaceFolders: vscode.workspace.workspaceFolders,
+    });
+    console.log({ repository });
+
+    const options = Object.entries(types).map(commitTypeToVsOption);
+    options[0].picked = true;
+
+    const { commitType } = await showQuickPick(options);
+
+    const affected = await showInputBox({
+      title: "Files or areas affected",
+      value: "entire project",
+      prompt: "e.g Main, Navbar",
+    });
+
+    const commitMessage = await showInputBox({
+      title: "Commit description",
+      prompt: "Write a short, imperative tense description of the change",
+      validateInput(commitDescription) {
+        return commitDescription.length === 0 && "subject is required";
+      },
+    });
+
+    repository.inputBox.value = formatCommit(
+      commitType,
+      affected,
+      commitMessage
+    );
+    await vscode.commands.executeCommand("git.commitAll", repository);
+
+    vscode.window.showInformationMessage("Hello World from bsmch-commits!");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log("registered");
   let disposable = vscode.commands.registerCommand(
-    "danbrima-bsmch-commits.add-commit",
+    "bsmch-commits.add-commit",
     commit
   );
 
